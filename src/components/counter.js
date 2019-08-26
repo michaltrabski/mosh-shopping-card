3e58ed9a0d31dc347a7a600d32a9d90434a2f521
Michal Trabski <michal.trabski@gmail.com>
2019-08-26 15:17:41 +0200

    added handleIncrement to counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,21 +2,40 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tag: ["t1", "t2", "t3"]
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-
     return (
       <>
         <div>
-          <span className={classes}>{this.formatCount()}</span>
-          <button className="btn btn-secondary btn-sm">Increment</button>
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <button
+            onClick={this.handleIncrement}
+            className="btn btn-secondary btn-sm"
+          >
+            Increment
+          </button>
+
+          <ul>
+            {this.state.tag.map(x => (
+              <li key={x}>{x}</li>
+            ))}
+          </ul>
         </div>
       </>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
